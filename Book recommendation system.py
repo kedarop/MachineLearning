3f0c33646d9a1036804c2f6d9f17b8306ef390c8
Kedar Patil <72346404+kedarop@users.noreply.github.com>
2024-12-23 11:39:03 +0530

    Update Book recommendation system.py

--- Book recommendation system.py
+++ Book recommendation system.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# BOOK RECOMMENDATION SYSTEM
-
-# In[3]:
-
-
 import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -56,7 +48,7 @@ data['average_rating'] = pd.to_numeric(data['average_rating'], errors='coerce')
 # In[9]:
 
 
-# Create a new column 'book_content' by combining 'title' and 'authors'
+
 data['book_content'] = data['title'] + ' ' + data['authors']
 
 
@@ -78,26 +70,26 @@ cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
 
 
 def recommend_books(book_title, cosine_sim=cosine_sim):
-    # Get the index of the book that matches the title
+    
     idx = data[data['title'] == book_title].index[0]
 
-    # Get the cosine similarity scores for all books with this book
+   
     sim_scores = list(enumerate(cosine_sim[idx]))
 
-    # Sort the books based on the similarity scores
+    
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
 
-    # Get the top 10 most similar books (excluding the input book)
+   
     sim_scores = sim_scores[1:11]
 
-    # Get the book indices
+   
     book_indices = [i[0] for i in sim_scores]
 
-    # Return the top 10 recommended books
+    
     return data['title'].iloc[book_indices]
 
 
-# In[13]:
+
 
 
 book_title = "Dubliners: Text  Criticism  and Notes"
@@ -105,7 +97,7 @@ recommended_books = recommend_books(book_title)
 print(recommended_books)
 
 
-# In[ ]:
+
 
 
 
